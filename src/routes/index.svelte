52b6c0731399ff170d29680adb618a490a61447f
Zac Miller-Waugh <z.millerwaugh@auckland.ac.nz>
2021-09-30 19:16:05 +1300

    Use AWS for icon hosting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -78,7 +78,7 @@
 	// Handle clicking on an icon
 	const selectIcon = (e) => {
 		const iconAlt = e.detail.alt;
-		const iconUrl = `https://flexiblelearning.auckland.ac.nz/icon-set/noun-project-icons/${
+		const iconUrl = `http://canvas-icons-static.s3-website-ap-southeast-2.amazonaws.com/${
 			e.detail.url
 		}?color=${colour.replace('#', '')}`;
 		const callback = $session.callback;
