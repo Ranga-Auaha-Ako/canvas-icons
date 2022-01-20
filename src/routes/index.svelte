e81b9fd93ca54781776f02f81643d3e1d210b3b4
Zac M-W <z.millerwaugh@auckland.ac.nz>
2022-01-20 14:35:40 +1300

    Version V2.5.1: Apply padding to other icon previews

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -159,7 +159,7 @@
 <svelte:head>
 	<!-- Inject lato font for Canvas preview -->
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&display=swap"
 		rel="stylesheet"
@@ -431,6 +431,7 @@
 												{showMargin}
 												{iconInBox}
 												{iconMargin}
+												{iconPadding}
 												{iconSize}
 												{colour}
 												{iconOffset}
@@ -444,6 +445,7 @@
 												{showMargin}
 												{iconInBox}
 												{iconMargin}
+												{iconPadding}
 												{iconSize}
 												{colour}
 												{iconOffset}
